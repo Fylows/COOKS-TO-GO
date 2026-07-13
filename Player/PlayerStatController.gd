e32096cf714b929609dffbed4ceed3f3125a7cbc
Ljiel Saplan <ldsaplan@up.edu.ph>
2026-07-13 18:02:58 +0800

    feat: add day event rolling

--- Player/PlayerStatController.gd
+++ Player/PlayerStatController.gd
@@ -8,3 +8,28 @@ func subtractMoney(money):
 
 func toggleUpgrade(upgrade):
 	return !upgrade
+
+func roll_post_day() -> void:
+	for key in post_day_events.keys():
+		var e = post_day_events[key]
+		var chance = clamp(e.base_chance + luck * e.luck_factor + daysPassed * e.day_factor, 0.0, 1.0)
+		e.active = randf() < chance
+
+func roll_pre_day() -> String:
+	var total_weight := 0.0
+	var weights := {}
+
+	for key in pre_day_events.keys():
+		var e = pre_day_events[key]
+		var w = max(0.0, e.base_weight + luck * e.luck_factor + daysPassed * e.day_factor)
+		weights[key] = w
+		total_weight += w
+
+	var roll = randf() * total_weight
+	for key in weights.keys():
+		if roll < weights[key]:
+			pre_day_events[key].active = true
+			return key
+		roll -= weights[key]
+
+	return ""
--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -17,15 +17,45 @@ var hasPalamig : bool = false
 # CIRCUMSTANCES
 
 # post day
-var nanakawan : bool = false
-var extraMoney : bool = false
-var SickChild : bool = false
+var post_day_events := {
+	"nanakawan": {
+		"active": false,
+		"type": "bad",
+		"base_chance": 0.2,
+		"luck_factor": -0.01,
+		"day_factor": 0.0
+	},
+	"extraMoney": {
+		"active": false,
+		"type": "good",
+		"base_chance": 0.2,
+		"luck_factor": 0.01,
+		"day_factor": 0.0
+	},
+	"sickChild": {
+		"active": false,
+		"type": "bad",
+		"base_chance": 0.2,
+		"luck_factor": -0.01,
+		"day_factor": 0.0
+	}
+}
 
 # pre day
-var willRain : bool =  false
-var awasan : bool = false
-
-
+var pre_day_events := {
+	"willRain": {
+		"active": false,
+		"base_weight": 1.0,
+		"luck_factor": 0.0,
+		"day_factor": 0.0
+	},
+	"awasan": {
+		"active": false,
+		"base_weight": 1.0,
+		"luck_factor": 0.0,
+		"day_factor": 0.0
+	}
+}
 # UPGRADES
 var palamigUP : bool = false
 var containerUP : bool = false
