41a5c7c5816b41ed23d53b454580ad2185863b74
Ljiel Saplan <ldsaplan@up.edu.ph>
2026-07-13 17:37:32 +0800

    feat: add PlayerStats.gd

---
+++ Player/PlayerStats.gd
@@ -0,0 +1,32 @@
+extends Node
+
+# PLAYER STATS
+
+var money : int = 100
+var luck : float = 1.0
+
+# RESOURSES
+
+var fishballStock : int = 0
+var kwekwekStock : int = 0
+var kikiamStock : int = 0
+var betamaxlStock : int = 0
+var hasPalamig : bool = false
+
+# CIRCUMSTANCES
+
+# post day
+var nanakawan : bool = false
+var extraMoney : bool = false
+var SickChild : bool = false
+
+# pre day
+var willRain : bool =  false
+var awasan : bool = false
+
+
+# UPGRADES
+var palamigUP : bool = false
+var containerUP : bool = false
+var cookUP : bool = false
+var burnUP : bool = false
