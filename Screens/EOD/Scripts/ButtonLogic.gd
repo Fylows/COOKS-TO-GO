be9da9f54a063ceb9781b087b3bcc730932f15ba
smmariquit <smmariquit@up.edu.ph>
2026-07-15 22:10:28 +0800

    show shop prices and money in Pesos

--- Screens/EOD/Scripts/ButtonLogic.gd
+++ Screens/EOD/Scripts/ButtonLogic.gd
@@ -30,7 +30,7 @@ func _ready() -> void:
 		$ResourceGroup/VBoxContainer/Kikiam.visible = false
 func _process(delta: float) -> void:
 	position = base_position + camera.offset * parallax_strength
-	$Stats/Money.text = str(PlayerStats.playerMoney)
+	$Stats/Money.text = PlayerStatController.format_pesos(PlayerStats.playerMoney)
 	var text = ("Palamig: %s" % PlayerStats.palamigStock) if PlayerStats.palamigUP else ""
 	$Stats/Resources.text = "Fishball: %d\nKikiam: %d\nKwek-Kwek: %d\nSauce: %s\n%s" % [PlayerStats.fishballStock, PlayerStats.kikiamStock, PlayerStats.kwekwekStock, PlayerStats.boughtSauce, text]
 	$Stats/Upgrades.text = "Upgrades\n\nPalamig: %s\nBigger Container: %s\nFaster Cooking: %s\nSlower Burning: %s" % [PlayerStats.palamigUP, PlayerStats.containerUP, PlayerStats.cookUP, PlayerStats.burnUP]
