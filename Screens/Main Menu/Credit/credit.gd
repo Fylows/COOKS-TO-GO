473320b8a183df052acd4f7ca04bc6bc53b3d800
smmariquit <smmariquit@up.edu.ph>
2026-07-15 22:09:23 +0800

    credits screen with menu bg and back button

--- Screens/Main Menu/Credit/credit.gd
+++ Screens/Main Menu/Credit/credit.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+const TITLE_SCENE := "res://Screens/Main Menu/Title_Screen/title_screen.tscn"
+
+
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu/Title_Screen/title_screen.tscn")
+	get_tree().change_scene_to_file(TITLE_SCENE)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_on_back_pressed()
